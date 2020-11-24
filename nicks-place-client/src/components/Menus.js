d36e89118bb62724652df2a273bdc2f3dbfa0df4
cesmiley <catherinesmiley@gmail.com>
2020-11-23 23:28:35 -0500

    add link to menu show page in menus component

--- nicks-place-client/src/components/Menus.js
+++ nicks-place-client/src/components/Menus.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Menus = ({ menus }) => {
     const menusList = menus.map((menu, index) => {
         return <div key={menu.id}>
-            <h2>{menu.name}</h2>
+            <Link 
+                to={`/menus/${menu.id}`}
+                ><h2>{menu.name}</h2></Link>
         </div>
     })
     return (
