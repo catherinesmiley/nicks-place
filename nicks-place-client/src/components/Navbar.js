fb651593143e09ae0bd6421b1b5d3a4580b9854a
cesmiley <catherinesmiley@gmail.com>
2020-11-23 22:49:09 -0500

    add home and meals links to navbar

--- nicks-place-client/src/components/Navbar.js
+++ nicks-place-client/src/components/Navbar.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <div>
-            
+            <NavLink 
+                to="/"
+                exact
+            >Home</NavLink>
+            <br />
+            <NavLink
+                to="/menus"
+                exact
+            >Menus</NavLink>
+            <br />
+            <br />
         </div>
     );
 };
