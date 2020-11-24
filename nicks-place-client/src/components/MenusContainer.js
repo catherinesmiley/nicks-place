106e7f3136175a564fcf7140981e52eeed40bd05
cesmiley <catherinesmiley@gmail.com>
2020-11-23 23:18:16 -0500

    add componentdidmount method to menus container component

--- nicks-place-client/src/components/MenusContainer.js
+++ nicks-place-client/src/components/MenusContainer.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { fetchMeals } from '../actions/meals'
+import { fetchMenus } from '../actions/menus'
 import { Switch, Route } from 'react-router-dom'
 
 import Meals from './Meals'
 import Menus from './Menus'
 
 class MenusContainer extends Component {
+
+    componentDidMount() {
+        this.props.fetchMenus()
+    }
+
     render() {
         return (
             <div>
@@ -20,6 +25,6 @@ class MenusContainer extends Component {
     }
 }
 
-const mapStateToProps = ({ meals }) => ({ meals })
+const mapStateToProps = ({ menus }) => ({ menus })
 
-export default connect(mapStateToProps, { fetchMeals })(MenusContainer);
+export default connect(mapStateToProps, { fetchMenus })(MenusContainer);
