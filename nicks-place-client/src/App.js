88bb994fc461876367e4b22f5d30d49f83fbde08
cesmiley <catherinesmiley@gmail.com>
2020-11-23 22:41:32 -0500

    create home and navbar components and add routing to app file

--- nicks-place-client/src/App.js
+++ nicks-place-client/src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
+import Home from './components/Home'
+import Navbar from './components/Navbar'
 import Menu from './components/Menu'
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <Menu />
-            </div>
+            <Router>
+                <div>
+                    <Navbar />
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                    </Switch>
+                    <Menu />
+                </div>
+            </Router>
+
         );
     }
 }
--- nicks-place-client/src/actions/meals.js
+++ nicks-place-client/src/actions/meals.js
@@ -2,6 +2,6 @@ export const fetchMeals = () => {
     return dispatch => {
         fetch('http://localhost:3090/meals')
         .then(resp => resp.json())
-        .then(meals => console.log(meals))
+        .then(meals => dispatch({ type: 'FETCH_MEALS', meals }))
     }
 }
