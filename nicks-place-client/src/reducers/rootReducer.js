1170ed36e08f135221c5c86b9a342fedbf892ad8
cesmiley <catherinesmiley@gmail.com>
2020-11-23 21:51:05 -0500

    set up index, app, and root reducer

---
+++ nicks-place-client/src/reducers/rootReducer.js
@@ -0,0 +1,19 @@
+export const rootReducer = (state = { categories: [], loading: false }, action) => {
+    switch (action.type) {
+        case 'LOADING_CATEGORIES': 
+        console.log("state in loading categories", state)
+            return {
+                ...state,
+                categories: [...state.categories],
+                loading: true
+            }
+        case 'ADD_CATEGORIES':
+            return {
+                ...state,
+                categories: action.categories,
+                loading: false
+            }
+        default: 
+            return state
+        }
+    }
