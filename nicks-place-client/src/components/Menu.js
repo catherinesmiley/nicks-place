ca572418f616f3a67d518d25619c2fbeb39affe4
cesmiley <catherinesmiley@gmail.com>
2020-11-23 23:47:40 -0500

    display menu info on menu show page

--- nicks-place-client/src/components/Menu.js
+++ nicks-place-client/src/components/Menu.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router-dom'
 
-const Menu = () => {
+const Menu = ({ menus }) => {
+    const { id } = useParams()
+    const menu = menus.find(m => m.id === parseInt(id))
+
+    if (menus.length === 0) return null 
+
     return (
         <div>
-            
+            <h2>{menu.name}</h2>
         </div>
     );
 };
