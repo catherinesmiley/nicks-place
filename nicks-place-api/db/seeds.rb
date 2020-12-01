503beefd0900293c17ca1e2b781a2c89180985bc
cesmiley <catherinesmiley@gmail.com>
2020-12-01 18:41:07 -0500

    create style sheet and set font

--- nicks-place-api/db/seeds.rb
+++ nicks-place-api/db/seeds.rb
@@ -8,7 +8,12 @@
 
 breakfast_menu = Menu.create(name: 'Breakfast Menu')
 breakfast_menu.meals.create([{ name: 'Biscuits & Gravy', description: 'Served with Country Potatoes or Grits, Choice of Sausage or Bacon', price: '$6.50' }, 
-{ name: 'Egg & Cheese Biscuit Sandwich', description: 'Served with Bacon or Sausage', price: '$3.50' }])
+{ name: 'Egg & Cheese Biscuit Sandwich', description: 'Served with Bacon or Sausage', price: '$3.50' },
+{ name: 'Eggs any Style', description: 'Two Eggs served with Country Potatoes or Grits, Choice of Sausage or Bacon, Toast', price: '$6.50' },
+{ name: 'Cheese Omelette', description: 'Filled with Sausage, Bacon or Ham, Served with Country Potatoes or Grits', price: '$7.50'},
+{ name: 'Pancake Breakfast', description: 'Served with Bacon or Sausage', price: '$6.50' },
+{ name: 'Breakfast Burrito', description: 'Tortilla filled with Scrambled Eggs, Potato, Cheese, Bacon or Sausage', price: '$3.50'},
+{ name: 'Breakfast Quesadilla', description: 'Scrambled Eggs, Cheese, Bacon or Sausage', price: '$4.50' }])
 
 lunch_menu = Menu.create(name: 'Lunch Menu')
 lunch_menu.menutypes.create([{ title: 'Salads' }, { title: 'Baskets & Plates' }, { title: 'Sandwiches & Burgers' }])
@@ -17,4 +22,8 @@ lunch_menu.meals.create([{ name: 'Quesadilla or Texas Burrito', description: 'Ch
 
 dinner_menu = Menu.create(name: 'Dinner Menu')
 dinner_menu.meals.create([{ name: 'Steak Salad', description: '8 oz. NY Strip Steak sliced on a bed of Mixed Greens with tomato, onion, cucumber and green pepper. Garnished with Crispy Fried Onions. Choice of Dressing', price: '$16' }, 
-{ name: 'Filet Mignon', description: '8 oz. Grilled Filet on a pool of Red Wine Demi-Glace, garnished with Crispy Fried Onions', price: '$20' }])
+{ name: 'Filet Mignon', description: '8 oz. Grilled Filet on a pool of Red Wine Demi-Glace, garnished with Crispy Fried Onions', price: '$20' },
+{ name: 'New York Strip Steak', description: '8 oz. Grilled Strip Steak, garnished with Crispy Fried Onions and Red Wine Demi-Glace', price: '$18' },
+{ name: 'Pan Seared Salmon', description: '8 oz. Fillet Seared with Garlic, Shallots, Summer Squash, Tomato, Capers, Onion & Cilantro, Garnished with a Citrus Cream Sauce', price: '$16' },
+{ name: 'Pan Seared Pork Tenderloin', description: '7 oz. Tenderloin with a Brandy Cream Sauce', price: '$15' },
+{ name: 'Four-Cheese Ravioli with Shrimp or Chicken', description: 'Tossed with garlic, fresh herbs, zucchini & yellow squash, Served with your choice of Sauce; Basil Plum Tomato Sauce or Parmesan Garlic Cream Sauce, Garlic Toast', price: '$16' }])
