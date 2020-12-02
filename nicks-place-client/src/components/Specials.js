24c6ab185a8878c7cdd9633e4043a2eabba6abb9
cesmiley <catherinesmiley@gmail.com>
2020-12-01 21:49:41 -0500

    add specials details

--- nicks-place-client/src/components/Specials.js
+++ nicks-place-client/src/components/Specials.js
@@ -6,9 +6,13 @@ const Specials = () => {
             <h1>Weekly Specials</h1>
             <h3>Sunday</h3>
             <h3>Tuesday</h3>
+            <p>Pork Chop - fried or grilled</p>
             <h3>Wednesday</h3>
+            <p>Meatloaf</p>
             <h3>Thursday</h3>
+            <p>Hamburger Steak</p>
             <h3>Friday</h3>
+            <p>Ribs</p>
             <h3>Saturday</h3>
         </>
     );
