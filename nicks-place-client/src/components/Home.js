f197c46cbdaf0544e57d889b93793b8be69f1875
cesmiley <catherinesmiley@gmail.com>
2020-12-01 20:50:40 -0500

    add logo to home

--- nicks-place-client/src/components/Home.js
+++ nicks-place-client/src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import nicksplacelogo from '../images/nicksplacelogo.jpg'
 
 const Home = () => {
     return (
         <div>
-            
+            <img src={nicksplacelogo} height="250" width="250" alt="logo" />;
         </div>
     );
 };
