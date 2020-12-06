3c1c34b7eb47570e7bf5e54ba48dc1b9f3156fcc
cesmiley <catherinesmiley@gmail.com>
2020-12-06 09:49:14 -0500

    add food allergy notice to menus component

--- nicks-place-client/src/components/Menus.js
+++ nicks-place-client/src/components/Menus.js
@@ -10,7 +10,16 @@ const Menus = ({ menus }) => {
         </div>
     })
     return (
-        <div>{menusList}</div>
+        <>
+            <div>{menusList}</div>
+            <br />
+            <div>
+                <h4>FOOD ALLERGY NOTICE</h4>
+                <p>PLEASE BE ADVISED THAT FOOD PREPARED HERE MAY CONTAIN THESE INGREDIENTS:
+                    <br /> 
+                    MILK, EGGS, WHEAT, SOYBEAN, PEANUTS, TREE NUTS, FISH AND SHELLFISH</p>
+            </div>
+        </>
     );
 };
 
