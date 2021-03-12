06b36a1db680c6024dd15d38bb72463f7964e5e5
catherinesmiley <catherinesmiley@gmail.com>
2021-03-11 20:00:19 -0500

    edit map component to include react-google-maps components and api key

--- nicks-place-client/src/components/Map.js
+++ nicks-place-client/src/components/Map.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 const Map = () => {
-    return (
-        <div>"hi!!!!"</div>
-    );
-};
+  
+  const mapStyles = {        
+    height: "100vh",
+    width: "100%"};
+  
+  const defaultCenter = {
+    lat: 41.3851, lng: 2.1734
+  }
+  
+  return (
+     <LoadScript
+       googleMapsApiKey='AIzaSyA3EIeAg6KMD07DFv4UQbAzbW142mrJSdQ'>
+        <GoogleMap
+          mapContainerStyle={mapStyles}
+          zoom={13}
+          center={defaultCenter}
+        />
+     </LoadScript>
+  )
+}
 
 export default Map;
