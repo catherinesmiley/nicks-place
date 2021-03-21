913f305d51943db7450cc580eb54091e3a16a389
catherinesmiley <catherinesmiley@gmail.com>
2021-03-21 19:21:24 -0400

    add on change event handler to text input and create on change function

--- nicks-place-client/src/components/Search.js
+++ nicks-place-client/src/components/Search.js
@@ -2,18 +2,29 @@ import React, { Component } from 'react';
 
 class Search extends Component {
 
+    state = ""
+
+    handleOnChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
     handleOnSubmit = event => {
         event.preventDefault()
         console.log("event submitted!")
     }
 
     render() {
+
+        console.log(this.state)
         return(
             <div>
                 <form onSubmit={event => this.handleOnSubmit(event)}>
                     <input
                         type="text"
                         placeholder="enter search here"
+                        onChange={this.handleOnChange}
                     />
                     <button>Search</button>
                 </form>
